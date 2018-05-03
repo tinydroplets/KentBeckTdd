bb52030bf7bc072bf67bd21c782ef15e45d0a8d4
tinydroplets <abdulla.mamuwala@gmail.com>
2018-05-03 09:57:55 -0500

    privacy

--- KentBeckTdd.UnitTests/GeneralTests.cs
+++ KentBeckTdd.UnitTests/GeneralTests.cs
@@ -9,10 +9,8 @@ namespace KentBeckTdd.UnitTests
         public void TestMultiplication()
         {
             var five = new Dollar(5);
-            var product = five.Times(2);
-            Assert.AreEqual(10, product.amount);
-            product = five.Times(3);
-            Assert.AreEqual(15, product.amount);
+            Assert.IsTrue(new Dollar(10).Equals(five.Times(2)));
+            Assert.IsTrue(new Dollar(15).Equals(five.Times(3)));
         }
 
         [TestMethod]
