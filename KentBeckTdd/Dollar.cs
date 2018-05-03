e9ff20570151edde965dd4ca51a5f52e35c9ca5a
tinydroplets <abdulla.mamuwala@gmail.com>
2018-05-03 08:56:53 -0500

    initial project setup

---
+++ KentBeckTdd/Dollar.cs
@@ -0,0 +1,17 @@
+﻿namespace KentBeckTdd
+{
+    public class Dollar
+    {
+        public int amount;
+
+        public Dollar(int amount)
+        {
+            this.amount = amount;
+        }
+
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(amount * multiplier);
+        }
+    }
+}
---
+++ UnitTestProject2/MultiplicationTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KentBeckTdd;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class MultiplicationTests
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            var five = new Dollar(5);
+            var product = five.Times(2);
+            Assert.AreEqual(10, product.amount);
+            product = five.Times(3);
+            Assert.AreEqual(15, product.amount);
+        }
+    }
+}
